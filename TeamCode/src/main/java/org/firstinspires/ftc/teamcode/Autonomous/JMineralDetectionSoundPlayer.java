5cf11a70078195d5a2290df23ab7ee2181583f97
hhhyyyfff <3067679349@qq.com>
2018-11-13 17:54:14 +0800

    Finished sound player

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JMineralDetectionSoundPlayer.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import android.media.MediaPlayer;
+
+import com.qualcomm.ftccommon.SoundPlayer;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.R;
+
+import java.util.List;
+
+public final class JMineralDetectionSoundPlayer extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        VuforiaLocalizer vuforia;
+        TFObjectDetector objectDetector;
+
+        VuforiaLocalizer.Parameters param = new VuforiaLocalizer.Parameters();
+        param.vuforiaLicenseKey = JAutonomousFinal_Shared.vuforiaLicenseKey;
+        param.cameraDirection = JAutonomousFinal_Shared.cameraDirection;
+        vuforia = ClassFactory.getInstance().createVuforia(param);
+
+        if (!ClassFactory.getInstance().canCreateTFObjectDetector()) throw new RuntimeException("TFOD not supported");
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = JAutonomousFinal_Shared.MINIMUM_CONFIDENCE;
+        objectDetector = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        objectDetector.loadModelFromAsset(JAutonomousFinal_Shared.TFOD_MODEL_ASSET,
+                JAutonomousFinal_Shared.LABEL_GOLD_MINERAL,
+                JAutonomousFinal_Shared.LABEL_SILVER_MINERAL);
+
+        telemetry.addData(this.toString(), "Ready to start");
+        telemetry.update();
+        waitForStart();
+
+        objectDetector.activate();
+        List<Recognition> updatedRecognitions;
+        while (true) {
+            updatedRecognitions = objectDetector.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                for ( Recognition recognition : updatedRecognitions ) {
+                    if (recognition.getLabel().equals(JAutonomousFinal_Shared.LABEL_GOLD_MINERAL) ) {
+                        SoundPlayer.getInstance().startPlaying(this.hardwareMap.appContext, R.raw.gold);
+                    } else if (recognition.getLabel().equals(JAutonomousFinal_Shared.LABEL_SILVER_MINERAL) ) {
+                        SoundPlayer.getInstance().startPlaying(this.hardwareMap.appContext, R.raw.silver);
+                    }
+                }
+            }
+            if (isStopRequested()) break;
+        }
+
+        objectDetector.shutdown();
+    }
+}
